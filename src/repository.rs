9454f5a1a98b0d5a19833be43518b13ed3492430
berserkkv <4121nnn@gmail.com>
2025-08-24 09:31:42 +0300

    preventing duplicates in table

--- src/repository.rs
+++ src/repository.rs
@@ -14,8 +14,11 @@ impl Repository {
         let conn = Connection::open(&db_path)?;
 
         conn.execute_batch("
+                DROP TABLE IF EXISTS bots;
+                DROP TABLE IF EXISTS orders;
+
                 CREATE TABLE IF NOT EXISTS bots (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id TEXT PRIMARY KEY,
                     name TEXT NOT NULL,
                     capital REAL NOT NULL,
                     start_time TEXT NOT NULL,
@@ -25,7 +28,7 @@ impl Repository {
                 );
 
                 CREATE TABLE IF NOT EXISTS orders (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id TEXT PRIMARY KEY,
                     symbol TEXT NOT NULL,
                     order_type TEXT NOT NULL,
                     bot_name TEXT NOT NULL,
@@ -52,9 +55,10 @@ impl Repository {
     pub fn create_bot(&self, bot: &Bot) -> Result<usize> {
         let conn = Connection::open(&self.path)?;
         let now = tools::get_date(3);
+        let id = format!("{}_{}", bot.name, bot.started_at);
         conn.execute(
-            "INSERT INTO bots (name, capital, wins, losses, start_time, end_time) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-            params![bot.name, bot.capital + bot.order_capital, bot.wins, bot.losses, bot.started_at.to_rfc3339(), now.to_rfc3339()],
+            "INSERT INTO bots (id, name, capital, wins, losses, start_time, end_time) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+            params![id, bot.name, bot.capital + bot.order_capital, bot.wins, bot.losses, bot.started_at.to_rfc3339(), now.to_rfc3339()],
         )
     }
 
@@ -133,12 +137,15 @@ impl Repository {
     pub fn create_orders(&self, orders: &Vec<Order>) -> Result<()> {
         let mut conn = Connection::open(&self.path)?;
 
+
+
         let tx = conn.transaction()?;
         {
-            let mut stmt = tx.prepare("INSERT INTO orders (symbol, order_type, bot_name, entry_price, exit_price, quantity, pnl, roe, created_at, closed_at, fee, leverage) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12)")?;
+            let mut stmt = tx.prepare("INSERT INTO orders (id, symbol, order_type, bot_name, entry_price, exit_price, quantity, pnl, roe, created_at, closed_at, fee, leverage) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13)")?;
 
             for o in orders {
-                stmt.execute(params![o.symbol, o.order_type, o.bot_name, o.entry_price, o.exit_price, o.quantity, o.pnl, o.roe, o.created_at.to_rfc3339(), o.closed_at.to_rfc3339(), o.fee, o.leverage])?;
+                let id = format!("{}_{}", o.bot_name, o.created_at);
+                stmt.execute(params![id, o.symbol, o.order_type, o.bot_name, o.entry_price, o.exit_price, o.quantity, o.pnl, o.roe, o.created_at.to_rfc3339(), o.closed_at.to_rfc3339(), o.fee, o.leverage])?;
             }
         }
 
